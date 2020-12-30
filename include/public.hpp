381dd7ebd836be0387b33acdf15dd94205dd1fe2
ofcZsgj <tcuZsgj@gmail.com>
2020-12-30 21:53:37 +0800

    Add group chat related messages types

--- include/public.hpp
+++ include/public.hpp
@@ -8,12 +8,15 @@
 // 消息类型
 enum EnMsgType
 {
-    LOGIN_MSG = 1,  // 1 登陆消息
-    LOGIN_MSG_ACK,  // 2 登陆响应消息
-    REG_MSG,        // 3 注册消息
-    REG_MSG_ACK,    // 4 注册响应消息
-    ONE_CHAT_MSG,   // 5 一对一聊天消息
-    ADD_FRIEND_MSG, // 6 添加好友信息
+    LOGIN_MSG = 1,    // 1 登陆消息
+    LOGIN_MSG_ACK,    // 2 登陆响应消息
+    REG_MSG,          // 3 注册消息
+    REG_MSG_ACK,      // 4 注册响应消息
+    ONE_CHAT_MSG,     // 5 一对一聊天消息
+    ADD_FRIEND_MSG,   // 6 添加好友信息
+    CREATE_GROUP_MSG, // 7 创建群组信息
+    ADD_GROUP_MSG,    // 8 加入群组信息
+    GROUP_CHAT_MSG,   // 9 群聊消息
 };
 
 #endif
